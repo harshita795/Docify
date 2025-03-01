48b38c32800bb79ff28ee94feaf1329f799a1f16
harshita795 <yadavharshita261303@gmail.com>
2025-03-01 14:39:53 +0530

    feat: docify models

---
+++ models/File.js
@@ -0,0 +1,51 @@
+const Folder = require("./Folder");
+module.exports = (sequelize, DataTypes) => {
+  const File = sequelize.define(
+    "File",
+    {
+      fileId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+      },
+      folderId: {
+        type: DataTypes.UUID,
+        references: {
+          model: Folder,
+          key: "folderId",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      uploadedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "files",
+      timestamps: false,
+    }
+  );
+
+  return File;
+};
